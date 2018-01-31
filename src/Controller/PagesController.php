a9051a4b2cdd3cabd26bfbaea6d2c15e73adb835
Alan Bonham <alan.bonham@hotmail.co.uk>
2018-01-31 13:09:07 +0000

    deletethiscommit

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -7,23 +7,28 @@
  */
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class SplashPageController extends Controller
+class PagesController extends Controller
 {
     /**
      * @Route("/welcome")
      */
-    public function welcome(){
+    public function splashPage(Request $request)
+    {
 
         $messages = array("Liam", "Alan", "Brad", "Paul");
         $welcomeMessage = $messages[array_rand($messages)];
 
-        return $this->render('app/SplashPage/Splashpage.html.twig');
+        return $this->render('app/SplashPage/Splashpage.html.twig', [
+            "message" => $welcomeMessage
+        ]);
     }
 }
+
 
